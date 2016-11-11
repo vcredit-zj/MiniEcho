678ef2c60c477551fb93144768fcff1f55d41ca4
User106 <qianchao321@yeah.net>
2016-11-11 16:45:04 +0800

    fix

---
+++ MiniEcho/MiniEcho/Project/Channel/Controllers/MEChannelViewController.h
@@ -0,0 +1,13 @@
+//
+//  MEChannelViewController.h
+//  MiniEcho
+//
+//  Created by vcredit on 16/11/11.
+//  Copyright © 2016年 vcredit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MEChannelViewController : UIViewController
+
+@end
