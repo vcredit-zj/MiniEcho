9fc624e27586a78591336e0cdad0f944e8c1df86
User106 <qianchao321@yeah.net>
2016-11-11 17:19:54 +0800

    a

---
+++ MiniEcho/MiniEcho/Project/Channel/Cells/ViewController1.h
@@ -0,0 +1,13 @@
+//
+//  ViewController1.h
+//  MiniEcho
+//
+//  Created by vcredit on 16/11/11.
+//  Copyright © 2016年 vcredit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController1 : UIViewController
+
+@end
---
+++ MiniEcho/MiniEcho/Project/Channel/Views/ViewController4.h
@@ -0,0 +1,13 @@
+//
+//  ViewController4.h
+//  MiniEcho
+//
+//  Created by vcredit on 16/11/11.
+//  Copyright © 2016年 vcredit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController4 : UIViewController
+
+@end
