841dedb21e67c022eadadb9a88a043663eda1eee
ALittleNasty <379643671@qq.com>
2016-11-11 16:57:11 +0800

    新增播放音乐的工具类

---
+++ MiniEcho/MiniEcho/Utility/MEPlayer.h
@@ -0,0 +1,15 @@
+//
+//  MEPlayer.h
+//  MiniEcho
+//
+//  Created by ALittleNasty on 16/11/11.
+//  Copyright © 2016年 vcredit. All rights reserved.
+
+/**
+ *  播放音乐的工具类
+ */
+#import <Foundation/Foundation.h>
+
+@interface MEPlayer : NSObject
+
+@end
---
+++ MiniEcho/MiniEcho/Utility/MEPlayer.m
@@ -0,0 +1,13 @@
+//
+//  MEPlayer.m
+//  MiniEcho
+//
+//  Created by ALittleNasty on 16/11/11.
+//  Copyright © 2016年 vcredit. All rights reserved.
+//
+
+#import "MEPlayer.h"
+
+@implementation MEPlayer
+
+@end
