0d863c3e536f8d779b9a7f99a57a689dab8159c3
ALittleNasty <379643671@qq.com>
2016-11-15 15:23:50 +0800

    添加图片素材

--- MiniEcho/MiniEcho/AppDelegate/AppDelegate.m
+++ MiniEcho/MiniEcho/AppDelegate/AppDelegate.m
@@ -13,6 +13,8 @@
 #import "ViewController1.h"
 #import <MMDrawerController.h> 
 
+#import "MEPlayMusicController.h"
+
 @interface AppDelegate ()
 
 {
@@ -50,8 +52,6 @@
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [self launchImageDisappear:launchImage];
     });
-
-    
     
     
     return YES;
@@ -72,6 +72,15 @@
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+
+    // 开始接受远程事件
+    [[UIApplication sharedApplication] beginReceivingRemoteControlEvents];
+    [self becomeFirstResponder];
+    
+    // 显示图片
+    if ([MEPlayer shareMEPlayer].isPlaying) {
+        
+    }
 }
 
 
@@ -82,6 +91,10 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    
+    // 结束接受远程事件
+    [[UIApplication sharedApplication] endReceivingRemoteControlEvents];
+    [self resignFirstResponder];
 }
 
 
@@ -89,5 +102,55 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+- (void)remoteControlReceivedWithEvent:(UIEvent *)event
+{
+    if (event.type == UIEventTypeRemoteControl) {
+        
+        switch (event.subtype) {
+            case UIEventSubtypeRemoteControlStop:{
+                NSLog(@"停止事件");
+            }
+                break;
+            case UIEventSubtypeRemoteControlTogglePlayPause:{
+                // 线控播放, 如果正在播放点击就暂停播放, 如果暂停点击就开始播放
+                
+                if ([MEPlayer shareMEPlayer].isPlaying) {
+                    // 暂停播放
+                    
+                }else{
+                    // 继续播放
+                }
+            }
+                break;
+                
+            case UIEventSubtypeRemoteControlPreviousTrack:{
+                // 上一曲
+                
+            }
+                break;
+                
+            case UIEventSubtypeRemoteControlNextTrack:{
+                //下一曲 以及耳机点击二次
+                
+            }
+                break;
+                
+            case UIEventSubtypeRemoteControlPlay:{
+                // 后台开始播放
+                
+            }
+                break;
+                
+            case UIEventSubtypeRemoteControlPause:{
+                //后台暂停播放
+                
+            }
+                break;
+                
+            default:
+                break;
+        }
+    }
+}
 
 @end
