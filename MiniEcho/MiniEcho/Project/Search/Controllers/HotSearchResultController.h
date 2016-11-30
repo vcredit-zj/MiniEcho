578fb17a12ef34e8553882c178b022a64e320af1
ALittleNasty <379643671@qq.com>
2016-11-30 14:32:44 +0800

    热点搜索完成部分

---
+++ MiniEcho/MiniEcho/Project/Search/Controllers/HotSearchResultController.h
@@ -0,0 +1,13 @@
+//
+//  HotSearchResultController.h
+//  MiniEcho
+//
+//  Created by ALittleNasty on 2016/11/25.
+//  Copyright © 2016年 vcredit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HotSearchResultController : UIViewController
+
+@end
